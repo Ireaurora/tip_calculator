22ef8e9e475bf0d572c8ec45508687fc0ed3e18c
Ireaurora <ireaurora@gmail.com>
2019-02-02 12:43:48 +0000

    Added a new page where a potential business can add their information and decided the tip rate. Carried out some research and found out it was 10% on average

--- tip_calculator.js
+++ tip_calculator.js
@@ -1,15 +1,3 @@
-
-
-window.test = function(e) {
-    if (e.value === "0") {
-        alert(e.value);
-
-    } else if (e.value === '1') {
-        alert(e.value);
-
-    }
-}
-
 function convert_to_Celsius(){
     let x = document.getElementById("mytext_from").value;
     let fahrenheit = (x*9/5) + 32;
