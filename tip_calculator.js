ed4a2ab3371c0190af5b673c4ec39c95471f5c4f
Ireaurora <ireaurora@gmail.com>
2019-02-06 08:38:17 +0000

    Added the calculation function which now works on calculating 10% of what the customer spent

--- tip_calculator.js
+++
@@ -1,6 +0,0 @@
-function convert_to_Celsius(){
-    let x = document.getElementById("mytext_from").value;
-    let fahrenheit = (x*9/5) + 32;
-    document.getElementById("mytext_to").value = fahrenheit;
-    document.getElementById("unit").value = "Celsius";
-}
