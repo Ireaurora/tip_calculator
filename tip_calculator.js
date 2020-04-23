d38718c194ae52f47e0b81ed1fd60f1c03f6af48
Ireaurora <ireaurora@gmail.com>
2020-04-23 20:09:20 +0100

    added some things

--- tip_calculator.js
+++ tip_calculator.js
@@ -6,14 +6,10 @@ function increasing_customer_satisfaction(){
     }
   }
 
-  // initially we can change the color of the background
-// This is the function that does the actual calulation based on the expense
 function tip_calculation() {
     var x = document.getElementById("expense").value;
     var tip = 0.10*x;
     document.getElementById("result").value = tip;
-    
-    //implementing a case statement to group the expense in a level
     }
    
     function readURL(){
@@ -28,20 +24,4 @@ function tip_calculation() {
          }
       }
       readURL();
- 
 
-      function previewFile(){
-        var preview = document.querySelector('img'); //selects the query named img
-        var file    = document.querySelector('input[type=file]').files[0]; //sames as here
-        var reader  = new FileReader();
- 
-        reader.onloadend = function () {
-            preview.src = reader.result;
-        }
-        if (file) {
-            reader.readAsDataURL(file); //reads the data as a URL
-        } else {
-            preview.src = "";
-        }
-   }
- previewFile();
