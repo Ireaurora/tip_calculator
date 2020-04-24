4743b2578d38ce55a48fb4cf0cee73752dd290e2
Ireaurora <ireaurora@gmail.com>
2020-04-24 09:34:09 +0100

    More clean-up

--- tip_calculator.js
+++ tip_calculator.js
@@ -1,26 +1,27 @@
+/*
 var customer_satisfaction = 0;
-
 function increasing_customer_satisfaction(){
-    for(var i = 0; i < wholereviews; i++){
-      customer_satisfaction++;
-    }
+  for(var i = 0; i < wholereviews; i++){
+    customer_satisfaction++;
   }
+}
+*/
 
 function tip_calculation() {
   //this is to be substituted with the formula required by the customer
-    var x = document.getElementById("expense").value;
-    var tip = 0.10*x;
-    document.getElementById("result").value = tip;
-    }
+  var x = document.getElementById("expense").value;
+  var tip = 0.10*x;
+  document.getElementById("result").value = tip;
+}
    
-    function readURL(){
-        const file1 = document.getElementById("background").files[0];
-        var reader1 = new FileReader();
-        reader1.onloadend = function(){
-           document.getElementById('section').style.backgroundImage = "url(" + reader1.result + ")";
-           document.getElementById('customizablepart').style.display ="none";
-        }
-        if(file1){reader1.readAsDataURL(file1);}
-      }
-      readURL();
+function readURL(){
+    const file1 = document.getElementById("background").files[0];
+    var reader1 = new FileReader();
+    reader1.onloadend = function(){
+      document.getElementById('section').style.backgroundImage = "url(" + reader1.result + ")";
+      document.getElementById('customizablepart').style.display ="none";
+    }
+    if(file1){reader1.readAsDataURL(file1);}
+}
+readURL();
 
